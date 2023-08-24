bfd84945ffa859e7c60d3fbdb1b4650537ebf16e
omar tarik yehya <khalaf.omar20@gmail.com>
2023-08-24 19:13:37 +0300

    Product list design

--- ecommerce-app/src/components/ProductList.js
+++ ecommerce-app/src/components/ProductList.js
@@ -1,10 +1,14 @@
+// src/components/ProductList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../ProductList.css';
+
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
+    // Fetch product data from the API
     axios.get('https://fakestoreapi.com/products')
       .then((response) => {
         setProducts(response.data);
@@ -12,18 +16,22 @@ const ProductList = () => {
       .catch((error) => {
         console.error('Error fetching product data:', error);
       });
-  }, []);
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
   return (
     <div>
-      <h2>Product List</h2>
-      <ul>
+      <h2 className='title'>Products</h2>
+      
+      <div className="product-list">
         {products.map((product) => (
-          <li key={product.id}>
-            <strong>{product.title}</strong> - ${product.price}
-          </li>
+          <div key={product.id} className="product-card">
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>Price: ${product.price}</p>
+            <p>Rating: {product.rating.rate} / 5</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
